8539f84bc22aebb4f2b66a9197c3a9b2927b8789
Sourav-Goyal19 <goyalsourav935@gmail.com>
2025-04-17 16:07:47 +0530

    second feature added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { AgriculturalSubsidyForm } from "./components/subsidy-form";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import LandingPage from "./components/home";
+import Chat from "./components/chat";
 
 function App() {
   const queryClient = new QueryClient();
@@ -13,6 +14,7 @@ function App() {
           <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/subsidy" element={<AgriculturalSubsidyForm />} />
+            <Route path="/chat" element={<Chat />} />
           </Routes>
         </Router>
       </div>
